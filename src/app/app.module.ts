e5b90359943833aefc7d52bc647596055eae3fb0
May <may0000ff@gmail.com>
2019-09-30 16:41:12 -0500

    created routes for navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FindRoute } from './FindRoute/FindRouteComponent';
+import { CreateReport } from './CreateReport/CreateReportComponent';
+import { ConnectContacts } from './ConnectToContacts/ConnectToContacts';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FindRoute,
+    CreateReport,
+    ConnectContacts
   ],
   imports: [
     BrowserModule,
