a4e057c8e184c5545970bbe1706e0bf6aa81c086
Carin Chapman <Chapman.carin@gmail.com>
2019-10-01 11:47:56 -0500

    Set up FindRouteComponents to communciate with backend

--- src/app/FindRoute/FindRouteComponent.ts
+++ src/app/FindRoute/FindRouteComponent.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service'
 @Component({
   selector: 'app-root',
   templateUrl: './FindRoute.html'
 })
-export class FindRoute {
-  
+export class FindRoute implements OnInit {
+    constructor(private http: HttpService) {}
+    
+    ngOnInit() {
+      this.http.myMethod()
+    }
 }
