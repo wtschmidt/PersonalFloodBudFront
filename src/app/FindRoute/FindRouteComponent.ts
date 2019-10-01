2618481bc6c16809e793358c0a1eacaa5bc67a9f
Carin Chapman <Chapman.carin@gmail.com>
2019-10-01 17:29:32 -0500

    Setup test routes just to see if communication with backend is working

--- src/app/FindRoute/FindRouteComponent.ts
+++ src/app/FindRoute/FindRouteComponent.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpService } from '../http.service'
+import { Component, OnInit, Injectable } from '@angular/core';
+import { HttpService } from '../http.service';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './FindRoute.html'
 })
 export class FindRoute implements OnInit {
+
+  brews: Object;
+
     constructor(private http: HttpService) {}
     
     ngOnInit() {
-      this.http.myMethod()
+      this.http.getRoute().subscribe(data => {
+        this.brews = data;
+        console.log(this.brews);
+      })
     }
 }
