51b228f9d8171451a0d4b02bae252aad0139a507
May <may0000ff@gmail.com>
2019-10-01 22:55:11 -0500

    rendering map in find route component

--- src/app/FindRoute/FindRouteComponent.ts
+++ src/app/FindRoute/FindRouteComponent.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { } from 'googlemaps';
 
 @Component({
   selector: 'app-root',
   templateUrl: './FindRoute.html'
 })
 export class FindRoute {
-  
+  @ViewChild('gmap', {static: true}) gmapElement: any;
+  map: google.maps.Map;
+
+  ngOnInit() {
+    var mapProp = {
+      center: new google.maps.LatLng(29.951065, -90.071533),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  }
 }
