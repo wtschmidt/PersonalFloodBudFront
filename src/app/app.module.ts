6bc94a2ebfb3288cc062bc2ff07382ed2e15afdc
Dan Murphy <dmurphy79@gmail.com>
2019-10-02 16:20:32 -0500

    imports BrowserAnimationsModule, MatDialogModule and DialogComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FindRoute } from './FindRoute/FindRouteComponent';
-import { CreateReport } from './CreateReport/CreateReportComponent';
-import { ConnectContacts } from './ConnectToContacts/ConnectToContacts';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FindRoute } from "./FindRoute/FindRouteComponent";
+import { CreateReport } from "./CreateReport/CreateReportComponent";
+import { ConnectContacts } from "./ConnectToContacts/ConnectToContacts";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
+import { HttpClientModule } from "@angular/common/http";
+import { MatDialogModule } from "@angular/material";
+import { DialogComponent } from "./dialog/dialog.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     FindRoute,
     CreateReport,
-    ConnectContacts
+    ConnectContacts,
+    DialogComponent
+    // CourseDialogComponent
   ],
+  entryComponents: [DialogComponent],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    MatDialogModule,
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
+  // entryComponents: [CourseDialogComponent]
 })
-export class AppModule { }
+export class AppModule {}
