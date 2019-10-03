45f1a1d10d8d02f03011e9aa532b4d919d3d19f8
Wesley <wtschmidt94@gmail.com>
2019-10-03 10:50:40 -0500

    get current GeoLocation in http.services

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -12,4 +12,25 @@ export class HttpService {
     // return this.http.get('https://api.openbrewerydb.org/breweries');
     return this.http.get('/route');
   }
+
+  currentPosition(position) {
+    const pos = {
+      lat : position.coords.latitude,
+      lng : position.coords.longitude
+    }
+    // let latLng = pos.lat + pos.lng;
+    // string version if we need it
+
+    return pos;
+  }
+
+  getLocation(){
+    // const latLng = lat.toString() + lng.toString();
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.currentPosition);
+    } else { 
+      alert("Geolocation is not supported by this browser.");
+    }
+    
+  }
 }
