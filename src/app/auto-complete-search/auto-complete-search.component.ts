08a65ad261ab1a11966083bc940a961f53d67bd2
May <may0000ff@gmail.com>
2019-10-03 16:56:06 -0500

    auto completes user input

--- src/app/auto-complete-search/auto-complete-search.component.ts
+++ src/app/auto-complete-search/auto-complete-search.component.ts
@@ -1,18 +1,37 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input, Output, ViewChild, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-auto-complete-search',
   templateUrl: './auto-complete-search.component.html'
 })
-export class AutoCompleteSearchComponent implements OnInit, AfterViewInit {
+export class AutoCompleteSearchComponent implements AfterViewInit {
 
-  constructor() { }
+  @Input() addressType: string;
+  @Output() setAddress: EventEmitter<any> = new EventEmitter();
+  @ViewChild('addresstext', {static: true}) addresstext: any;
 
-  ngOnInit() {
+  autocompleteInput: string;
+
+  constructor() {
   }
 
   ngAfterViewInit() {
-    
+    this.getPlaceAutocomplete();
   }
 
+  private getPlaceAutocomplete() {
+    const autocomplete = new google.maps.places.Autocomplete(this.addresstext.nativeElement,
+      {
+        componentRestrictions: { country: 'US' },
+        types: [this.addressType]  // 'establishment' / 'address' / 'geocode'
+      });
+    google.maps.event.addListener(autocomplete, 'place_changed', () => {
+      const place = autocomplete.getPlace();
+      this.invokeEvent(place);
+    });
+  }
+
+  invokeEvent(place: Object) {
+    this.setAddress.emit(place);
+  }
 }
