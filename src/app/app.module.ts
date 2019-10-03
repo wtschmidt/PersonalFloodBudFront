f29d51befd1aca45a19e0194e5f2fffaaf096e8c
Dan Murphy <dmurphy79@gmail.com>
2019-10-03 16:59:41 -0500

    adds more structure to reporting page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FindRoute } from './FindRoute/FindRouteComponent';
-import { CreateReport } from './CreateReport/CreateReportComponent';
-import { ConnectContacts } from './ConnectToContacts/ConnectToContacts';
-import { AutoCompleteSearchComponent } from './auto-complete-search/auto-complete-search.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FindRoute } from "./FindRoute/FindRouteComponent";
+import { CreateReport } from "./CreateReport/CreateReportComponent";
+import { ConnectContacts } from "./ConnectToContacts/ConnectToContacts";
+import { AutoCompleteSearchComponent } from "./auto-complete-search/auto-complete-search.component";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
+import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { UserLocationComponent } from './user-location/user-location.component';
+import { UserLocationComponent } from "./user-location/user-location.component";
 import {
   MatButtonModule,
   MatIconModule,
   MatToolbarModule,
   MatMenuModule,
-  MatDialogModule
+  MatDialogModule,
+  MatCardModule,
+  MatInputModule
 } from "@angular/material";
 import { DialogComponent } from "./dialog/dialog.component";
 import { DialogService } from "./services/dialog.service";
-import { AppHeaderComponent } from './app-header/app-header.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AppHeaderComponent } from "./app-header/app-header.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
 
 @NgModule({
   declarations: [
@@ -50,6 +53,9 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
     MatToolbarModule,
     MatMenuModule,
     MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
     ServiceWorkerModule.register("ngsw-worker.js", {
       enabled: environment.production
     })
