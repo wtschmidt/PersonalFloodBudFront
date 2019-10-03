849ffe139a8426e41f70f7b6b667e1d34ced84b3
May <may0000ff@gmail.com>
2019-10-03 10:41:51 -0500

    hardcoded routes rendering on map

--- src/app/FindRoute/FindRouteComponent.ts
+++ src/app/FindRoute/FindRouteComponent.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, ElementRef } from '@angular/core';
 import { } from 'googlemaps';
 import { HttpService } from '../http.service';
 import { Observable } from 'rxjs';
@@ -8,16 +8,51 @@ import { Observable } from 'rxjs';
   templateUrl: './FindRoute.html'
 })
 export class FindRoute implements OnInit {
+
   @ViewChild('gmap', {static: true}) gmapElement: any;
+  directionsService: any;
+  directionsRenderer: any;
+  origin = new google.maps.LatLng(29.9855777, -90.08403059999999);
+  destination = new google.maps.LatLng(29.951065, -90.071533);;
   map: google.maps.Map;
-  constructor(private http: HttpService) { }
+
+  constructor(private http: HttpService) {
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsRenderer = new google.maps.DirectionsRenderer();
+  }
 
   ngOnInit() {
     var mapProp = {
-      center: new google.maps.LatLng(29.951065, -90.071533),
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+      zoom: 14,
+      center: this.origin
+    }
+    // this.userLocation();
     this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+    this.directionsRenderer.setMap(this.map);
+  }
+  // userLocation() {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(function (position) {
+  //       var geolocation = {
+  //         lat: position.coords.latitude,
+  //         lng: position.coords.longitude
+  //       };
+  //       console.log(geolocation);
+  //     })
+  //   }
+  // }
+  calcRoute() {
+    var request = {
+      origin: this.origin,
+      destination: this.destination,
+      travelMode: google.maps.TravelMode['DRIVING']
+    };
+
+    let myDirectionsRenderer = this.directionsRenderer
+    this.directionsService.route(request, function (response) {
+      if (response.status == 'OK') {
+        myDirectionsRenderer.setDirections(response);
+      }
+    });
   }
 }
