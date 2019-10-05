7c2598c293458d90266bb046d45b5ffe62aa06e9
Carin Chapman <Chapman.carin@gmail.com>
2019-10-05 12:21:56 -0500

    Declare the variable rainfall in HttpService, to pass to component where it's being used

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -7,7 +7,8 @@ import { HttpClient } from '@angular/common/http'
 export class HttpService {
 
   constructor(private http: HttpClient) { }
-
+  rainfall: object;
+  
   getRoute(){
     return this.http.get('/route');
   }
