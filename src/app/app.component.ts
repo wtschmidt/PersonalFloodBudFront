217ceeb3529507c5bbfaae87d73e8ce1230caf7d
Carin Chapman <Chapman.carin@gmail.com>
2019-10-05 12:20:55 -0500

    Import HttpService, add call to getRainfall cuntion inside ngOnInIt in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { HttpService } from './http.service';
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = "flood";
 
-  constructor() {}
+  constructor(private http: HttpService) {}
+
+  ngOnInit() {
+    this.http.getRainfall().subscribe(data => {
+        console.log(data + ' is the rainfall so far today');
+        this.http.rainfall = data;
+
+  })
+}
 }
