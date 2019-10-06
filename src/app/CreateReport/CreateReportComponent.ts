69de525e5f7c7988622a8e542e222562df28ca96
Wesley <wtschmidt94@gmail.com>
2019-10-06 16:56:37 -0500

    tried to add second marker

--- src/app/CreateReport/CreateReportComponent.ts
+++ src/app/CreateReport/CreateReportComponent.ts
@@ -17,6 +17,8 @@ export class CreateReport implements OnInit {
   directionsRenderer: any;
   map: google.maps.Map;
   marker: google.maps.Marker;
+  marker2: google.maps.Marker;
+  markers = [{lat: 29.9777, lng: -90.0797473}, {lat: 29.9797, lng: -90.0777473}, {lat: 29.9770, lng: -90.0797773}, {lat: 29.9699, lng: -90.08}]
   lat = 0;
   lng = 0;
   latLng = {};
@@ -32,11 +34,9 @@ export class CreateReport implements OnInit {
       zoom: 12,
       center: new google.maps.LatLng(29.95, -90.05),
     }
-    // this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
     this.lat = this.geo.currLat;
     this.lng = this.geo.currLng;
-    console.log("CLICK CLICK BOOM", this.lat);
-    console.log("CLICK CLICK BOOM GEo", this.geo.currLat);
 
     // this.geo.getLocation();
     // console.log(this.geo.currLat);
@@ -48,8 +48,29 @@ export class CreateReport implements OnInit {
         zoom: 18,
         center: new google.maps.LatLng(this.geo.currLat, this.geo.currLng),
       }),
+      label: "A",
       title: 'Save us... bitch!'
     });
+    // this.marker = new google.maps.Marker({
+    //   position: new google.maps.LatLng(29.9777, -90.0797473),
+    //   map: new google.maps.Map(this.gmapElement.nativeElement, {
+    //     zoom: 18,
+    //     center: new google.maps.LatLng(this.geo.currLat, this.geo.currLng),
+    //   }),
+    //   label: "B",
+    //   title: 'Lesser Bitch!'
+    // });
+    // this.markers.forEach(mark => {
+    //   this.marker = new google.maps.Marker({
+    //   position: new google.maps.LatLng(mark.lat, mark.lng),
+    //   map: new google.maps.Map(this.gmapElement.nativeElement, {
+    //     zoom: 18,
+    //     center: new google.maps.LatLng(this.geo.currLat, this.geo.currLng),
+    //   }),
+    //   label: "B",
+    //   title: 'lesser bitches!'
+    // });
+    // })
   }
 
   submitReport() {
