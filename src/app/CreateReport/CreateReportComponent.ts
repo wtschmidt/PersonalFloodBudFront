e0c8770e21dbf30d39d2d25a7f2f4d8cc279a036
May <may0000ff@gmail.com>
2019-10-06 03:38:44 -0500

    added agm map and auto search(commented out)

--- src/app/CreateReport/CreateReportComponent.ts
+++ src/app/CreateReport/CreateReportComponent.ts
@@ -3,6 +3,7 @@ import { MatCardModule } from "@angular/material";
 import { HttpService } from "../http.service";
 import {} from "googlemaps";
 import { UserLocationService } from "../services/user-location.service";
+import { AutoSearchComponent } from '../auto-search/auto-search.component'; 
 
 @Component({
   selector: "create-report",
@@ -11,69 +12,35 @@ import { UserLocationService } from "../services/user-location.service";
 })
 export class CreateReport implements OnInit {
 
-  currLat: number;
-  currLng: number;
-  description: string;
-  location: string;
-  @ViewChild("gmap", { static: true }) gmapElement: any;
-  // @ViewChild(UserLocationComponent, {static: true}) geo;
-  directionsService: any;
-  directionsRenderer: any;
-  map: google.maps.Map;
-  marker: google.maps.Marker;
-  lat = 0;
-  lng = 0;
-  // description: string;
-  // location: string;
+  @ViewChild(AutoSearchComponent, { static: true }) AutoSearch;
+  markLat: number;
+  markLng: number;
   image: any;
   report = {
-    latLng: this.geo.currLat + "," + this.geo.currLng,
+    latLng: this.markLat + "," + this.markLng,
     location: '',
     desc: '',
     img: this.image,
-  }
+  };
 
-  constructor(private http: HttpService, private geo: UserLocationService) {}
+  constructor(private http: HttpService, private geo: UserLocationService) { }
 
   ngOnInit() {
-    this.currLat = this.geo.currLat;
-    this.currLng = this.geo.currLng;
-  }
-
-  submitReport() {
-    this.http.getAddress(this.geo.currLat, this.geo.currLng);
-    this.geo.getLocation();
-    var mapProp = {
-      zoom: 8,
-      center: new google.maps.LatLng(29.95, -90.05)
-    };
-    // this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
-    this.lat = this.geo.currLat;
-    this.lng = this.geo.currLng;
-    console.log("CLICK CLICK BOOM", this.lat);
-    console.log("CLICK CLICK BOOM GEo", this.geo.currLat);
-
-    // this.geo.getLocation();
-    // console.log(this.geo.currLat);
-    // this.latLng = {lat: this.geo.currLat, lng: this.geo.currLng};
-
-    this.marker = new google.maps.Marker({
-      position: new google.maps.LatLng(this.geo.currLat, this.geo.currLng),
-      map: new google.maps.Map(this.gmapElement.nativeElement, {
-        zoom: 14,
-        center: new google.maps.LatLng(this.geo.currLat, this.geo.currLng)
-      }),
-      title: "Save us... bitch!"
-    });
+    this.markLat = this.geo.currLat;
+    this.markLng = this.geo.currLng;
   }
 
   createReport() {
+    console.log(this.report);
     this.http.submitReport(this.report).subscribe(data => {
       console.log(data);
     });
   }
-  
 
+  // currLat: number;
+  // currLng: number;
+  // description: string;
+  // location: string;
   // @ViewChild("gmap", { static: true }) gmapElement: any;
   // // @ViewChild(UserLocationComponent, {static: true}) geo;
   // directionsService: any;
@@ -82,16 +49,28 @@ export class CreateReport implements OnInit {
   // marker: google.maps.Marker;
   // lat = 0;
   // lng = 0;
-  // latLng = {};
-  // description: string;
-  // location: string;
+  // // description: string;
+  // // location: string;
+  // image: any;
+  // report = {
+  //   latLng: this.geo.currLat + "," + this.geo.currLng,
+  //   location: '',
+  //   desc: '',
+  //   img: this.image,
+  // }
 
   // constructor(private http: HttpService, private geo: UserLocationService) {}
 
   // ngOnInit() {
+  //   this.currLat = this.geo.currLat;
+  //   this.currLng = this.geo.currLng;
+  // }
+
+  // submitReport() {
+  //   this.http.getAddress(this.geo.currLat, this.geo.currLng);
   //   this.geo.getLocation();
   //   var mapProp = {
-  //     zoom: 12,
+  //     zoom: 8,
   //     center: new google.maps.LatLng(29.95, -90.05)
   //   };
   //   // this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
@@ -107,35 +86,16 @@ export class CreateReport implements OnInit {
   //   this.marker = new google.maps.Marker({
   //     position: new google.maps.LatLng(this.geo.currLat, this.geo.currLng),
   //     map: new google.maps.Map(this.gmapElement.nativeElement, {
-  //       zoom: 18,
+  //       zoom: 14,
   //       center: new google.maps.LatLng(this.geo.currLat, this.geo.currLng)
   //     }),
   //     title: "Save us... bitch!"
   //   });
   // }
 
-  // submitReport() {
-  //   // function that converts our lat/long into an address
-  //   // console.log(this.description);
-  //   console.log(this.lat);
-  //   this.http.getAddress(this.geo.currLat, this.geo.currLng);
-  // }
-
-  // getCurrentLocation() {
-  //   // this.lat = this.geo.currLat;
-  //   // this.lng = this.geo.currLng;
-  //   // console.log("CLICK CLICK BOOM", this.lat);
-  //   // console.log("CLICK CLICK BOOM GEo", this.geo.currLat);
-  //   // // this.geo.getLocation();
-  //   // // console.log(this.geo.currLat);
-  //   // // this.latLng = {lat: this.geo.currLat, lng: this.geo.currLng};
-  //   // this.marker = new google.maps.Marker({
-  //   //   position: new google.maps.LatLng(this.geo.currLat, this.geo.currLng),
-  //   //   map: new google.maps.Map(this.gmapElement.nativeElement, {
-  //   //     zoom: 18,
-  //   //     center: new google.maps.LatLng(this.geo.currLat, this.geo.currLng),
-  //   //   }),
-  //   //   title: 'Save us... bitch!'
-  //   // });
+  // createReport() {
+  //   this.http.submitReport(this.report).subscribe(data => {
+  //     console.log(data);
+  //   });
   // }
 }
