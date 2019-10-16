6d4a5f3ad04f96aff18831f560b8da0ae920209a
Wesley <wtschmidt94@gmail.com>
2019-10-15 19:43:36 -0500

    tried fixing land in ocean problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit {
       console.log('ROUTE', this.activatedRoute.snapshot.queryParams);
     }
 
-  ngOnInit() {
+  async ngOnInit() {
     // const userId = this.activatedRoute.snapshot.queryParams.id;
     // console.log(userId);
     // if (userId) {
@@ -30,7 +30,7 @@ export class AppComponent implements OnInit {
       localStorage.setItem('userId', queryParams.get("id"));
     })
 
-    this.geo.getLocation();
+    await this.geo.getLocation();
 
     this.http.getReports().subscribe(data => {
       this.http.dbReports = data;
